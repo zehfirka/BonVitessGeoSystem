e8e537e4e517cb53e49d95221b19cb9f050e5fe7
zehfirka <franchuk.viktor@lll.kpi.ua>
2022-12-11 13:27:37 +0200

    Redesigned UI + bugfix

--- app/src/main/java/ua/pack/bonvitess/aboutUs.java
+++ app/src/main/java/ua/pack/bonvitess/aboutUs.java
@@ -2,7 +2,10 @@ package ua.pack.bonvitess;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class aboutUs extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class aboutUs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+    }
+
+    public void onFirstViewCLick(View view) {
+        Intent firstView = new Intent(Intent.ACTION_VIEW, Uri.parse("https://instagram.com/klymenko_ruslana_?igshid=YmMyMTA2M2Y="));
+        startActivity(firstView);
+    }
+    public void onSecondViewClick(View view) {
+        Intent secondView = new Intent(Intent.ACTION_VIEW,Uri.parse("https://instagram.com/b0nnechane?igshid=YmMyMTA2M2Y="));
+        startActivity(secondView);
+    }
+    public void onThirdViewClick(View view) {
+        Intent thirdView = new Intent(Intent.ACTION_VIEW,Uri.parse("https://instagram.com/zehfirka?igshid=YmMyMTA2M2Y="));
+        startActivity(thirdView);
+    }
+    public void onFourthViewClick(View view) {
+        Intent fourthView = new Intent(Intent.ACTION_VIEW,Uri.parse("https://instagram.com/_.roman.roman._?igshid=YmMyMTA2M2Y="));
+        startActivity(fourthView);
+    }
+    public void onFifthViewClick(View view) {
+        Intent fifthView = new Intent(Intent.ACTION_VIEW,Uri.parse("https://instagram.com/pnmrnkko?igshid=YmMyMTA2M2Y="));
+        startActivity(fifthView);
     }
 }
